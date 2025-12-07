ff535b3b57fd3f3b27a44c92b1ded313bc58662a
jolantadjatlova <jolanta.djatlova@icloud.com>
2025-12-07 15:14:34 +0000

    Add working homepage template with static CSS & background image

--- easy_eats/settings.py
+++ easy_eats/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=kp23!f*&2-+k)&w&^(j@ct(a5b-_)c-s@=1wv442=v9vdcl2*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Welcome to Easy Eats Kitchen!")
+def home(request):
+    """
+    Home page view for Easy Eats Kitchen.
+    Renders the main landing page using the base template.
+    
+    """
+
+    return render(request, 'recipes/base.html')
