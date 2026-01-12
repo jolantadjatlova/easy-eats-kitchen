d3200a73cd1cfd6ca3889af4b0e0d92e617bdae9
jolantadjatlova <jolanta.djatlova@icloud.com>
2026-01-12 21:26:39 +0000

    Update navbar to match wireframes and configure auth redirects

--- easy_eats/settings.py
+++ easy_eats/settings.py
@@ -129,3 +129,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "recipes:my_recipes"
+LOGOUT_REDIRECT_URL = "recipes:home"
+LOGIN_URL = "login"
