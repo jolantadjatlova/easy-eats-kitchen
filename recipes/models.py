8a53bacf3f8fb96b7b17d6665a54a3f321b8cfc2
jolantadjatlova <jolanta.djatlova@icloud.com>
2026-01-13 13:10:52 +0000

    Add Category model and admin registration

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=60, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
