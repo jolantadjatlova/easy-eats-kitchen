17110e6059e59dcc49efe361c97ca63b116f2757
jolantadjatlova <jolanta.djatlova@icloud.com>
2026-01-14 22:12:53 +0000

    Fix recipe display issues, secure settings, and untrack database file

--- easy_eats/settings.py
+++ easy_eats/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=kp23!f*&2-+k)&w&^(j@ct(a5b-_)c-s@=1wv442=v9vdcl2*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
