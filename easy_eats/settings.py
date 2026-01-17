61f3994a3a5d943201cccb1f2a7fd0c2c73d08aa
jolantadjatlova <jolanta.djatlova@icloud.com>
2026-01-17 19:48:10 +0000

    Add Summernote for recipe admin

--- easy_eats/settings.py
+++ easy_eats/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_summernote',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Category, Recipe
 
 # Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    summernote_fields = ("ingredients", "method")
+
 admin.site.register(Category)
-admin.site.register(Recipe)
